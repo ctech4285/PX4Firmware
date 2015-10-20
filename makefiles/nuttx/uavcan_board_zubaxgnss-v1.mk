a50afe4a47068f11933d9a1f6b5c1411cef054e2
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-10-20 13:23:26 +0300

    Zubax GNSS BL HW Version update

--- makefiles/nuttx/uavcan_board_zubaxgnss-v1.mk
+++ makefiles/nuttx/uavcan_board_zubaxgnss-v1.mk
@@ -2,6 +2,6 @@
 # Uavcan hardware version definitions for the zubax gnss
 #
 
-export UAVCANBLID_HW_VERSION_MAJOR=1
+export UAVCANBLID_HW_VERSION_MAJOR=2
 export UAVCANBLID_HW_VERSION_MINOR=0
 export UAVCANBLID_NAME= "\"com.zubax.gnss\""
